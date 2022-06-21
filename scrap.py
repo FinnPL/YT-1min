7c18afaf46b0ac9f2247c34361e9a58817b22395
Finn Paul Lippok <finnpaullippok@outlook.de>
2022-06-21 18:05:14 +0200

    added get_recommendation and check_vidio_length

--- scrap.py
+++ scrap.py
@@ -1,17 +1,53 @@
 #scrap youtube recommendation of a video
+from threading import main_thread
 from requests_html import HTMLSession 
 from bs4 import BeautifulSoup as bs
+import re
+import json
+import urllib
+import config
 
 
-video_url = "https://www.youtube.com/watch?v=xqFTe96OWPU"
 
-# init an HTML Session
-session = HTMLSession()
-# get the html content
-response = session.get(video_url)
-# execute Java-script
-response.html.render(sleep=1)
-# create bs object to parse HTML
-soup = bs(response.html.html, "html.parser")
 
-print (soup.find_all("meta"))
+def get_recommendation(video_url):
+    # init an HTML Session
+    session = HTMLSession()
+    # get the html content
+    response = session.get(video_url)
+    # execute Java-script
+    response.html.render(sleep=1)
+    # create bs object to parse HTML
+    soup = bs(response.html.html, "html.parser")
+
+    recom_ids = list()
+    recom_ids.append('video_url')
+
+    for a in soup.find_all('a', href=True):
+        x = a['href']
+        if re.search("/watch\?v=", x):
+            x=x.replace("/watch?v=", "")
+            if x not in recom_ids:
+                recom_ids.append(x)
+    
+    recom_ids.remove('video_url')
+    return recom_ids
+
+def check_vidio_length(video_id):
+    searchUrl="https://www.googleapis.com/youtube/v3/videos?id="+video_id+"&key="+config.api_key+"&part=contentDetails"
+    response = urllib.request.urlopen(searchUrl).read()
+    data = json.loads(response)
+    all_data=data['items']
+    contentDetails=all_data[0]['contentDetails']
+    duration=contentDetails['duration']
+    print (duration)
+
+
+def main():
+    list = get_recommendation('https://www.youtube.com/watch?v=xqFTe96OWPU')
+    print(list)
+    for i in list:
+        check_vidio_length(i)
+
+if __name__ == "__main__":
+    main()
